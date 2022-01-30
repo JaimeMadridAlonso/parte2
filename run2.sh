b2ead56f740677735b4ef4a6e7b9b8a944559664
JaimeMadridAlonso <36418914+JaimeMadridAlonso@users.noreply.github.com>
2022-01-30 19:21:24 +0100

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -2,6 +2,6 @@
 
 export GROUP_NUMBER=48
 
-sudo docker build -f Dockerfile -t 48/productpage .
+docker build -f Dockerfile -t 48/productpage .
 
-sudo docker run -p 9080:9080 48/productpage
+docker run -p 9080:9080 48/productpage
